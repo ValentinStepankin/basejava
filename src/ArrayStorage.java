0bb78f111d17979e6c7771f4a0da3eecb29d1b1d
Stepankin Valentin <vallentinnnnn546@gmail.com>
2022-03-21 17:26:37 +0300

    Add realization for all methods in ArrayStorage class

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,69 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                storage[i] = null;
+            }
+        }
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume resume = null;
+        for (Resume value : storage) {
+            if (value != null && value.uuid == uuid) {
+                resume = value;
+                break;
+            }
+        }
+        return resume;
     }
 
     void delete(String uuid) {
+        int indexOfElement = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null && storage[i].uuid == uuid) {
+               indexOfElement = i;
+               break;
+            }
+        }
+        for (int i = indexOfElement; i < storage.length-1; i++){
+            storage[i] = storage[i+1];
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int count = 0;
+        for (Resume resume : storage) {
+            if (resume != null) count++;
+        }
+
+        Resume[] testStorage = new Resume[count];
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                testStorage[i] = storage[i];
+            }
+        }
+        return testStorage;
     }
 
     int size() {
-        return 0;
+        int count = 0;
+        for (Resume resume : storage) {
+            if (resume != null) count++;
+        }
+        return count;
     }
 }
